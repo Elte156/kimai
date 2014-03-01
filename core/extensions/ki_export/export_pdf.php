16973f5236e2a7aad0c246e72d043ac658a8983a
Elte156 <Elte156@gmail.com>
2014-03-01 18:10:25 -0500

    changed date_format multi-dimensional array keys from a string to an int

--- core/extensions/ki_export/export_pdf.php
+++ core/extensions/ki_export/export_pdf.php
@@ -222,7 +222,7 @@ $pdf->WriteHtml('<h1>' . $pdf_title . '</h1>');
 $pdf->ln();
 
 $pdf->WriteHtml('<b>'.$kga['lang']['export_extension']['time_period'].':</b> '.
-strftime($kga['date_format']['2'],$in).' - '.strftime($kga['date_format']['2'],$out) );
+strftime($kga['date_format'][2],$in).' - '.strftime($kga['date_format'][2],$out) );
 
 if (isset($pdf_filter)) {
   $pdf->ln();
